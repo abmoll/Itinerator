c54e86875f65331ef7fb8688e348253f9608dd10
ALEX MOLL <alexmoll@ALEXs-MacBook-Pro.local>
2017-12-11 15:45:12 -0700

    new updates

--- public/main.js
+++ public/main.js
@@ -1,17 +1,437 @@
-var GoogleMapsJsAPI = AIzaSyCOK8unHwKjh44byRIfMgAuRBpoH63_CqE
-var GoogleMapsDirectionsAPI = AIzaSyBna-nft5cxYdVqD4vHCgurCqhi3B9zNhY
-var GoogleMapsPlacesAPI = AIzaSyAGzhtIF6wqZ96WemXVfDdg3TerD_TYyiU
-var eventfulKey = pcXqHp3KG3jmtgNJ
+//var GoogleMapsJsAPI = AIzaSyCOK8unHwKjh44byRIfMgAuRBpoH63_CqE
+//var GoogleMapsDirectionsAPI = AIzaSyBna-nft5cxYdVqD4vHCgurCqhi3B9zNhY
+//var GoogleMapsPlacesAPI = AIzaSyAGzhtIF6wqZ96WemXVfDdg3TerD_TYyiU
+//var eventfulKey = pcXqHp3KG3jmtgNJ
 //var API = http://api.eventful.com/json/events/search?...&location=San+Diego
+//var hikingProjectAPI = 200192113-0e12500ca3d4423414d88aaa658cda2e
+//var https://www.hikingproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&key=200192113-0e12500ca3d4423414d88aaa658cda2e
 
-  function initMap() {
-    var boulder = {lat: 40.017, lng: -105.28};
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 12,
-      center: boulder
+// initialize map on webpage
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: countries['us'].zoom,
+    center: countries['us'].center,
+    mapTypeControl: false,
+    panControl: false,
+    zoomControl: false,
+    streetViewControl: false
+  });
+}
+
+var map, places, infoWindow;
+var markers = [];
+var autocomplete;
+var countryRestrict = {
+  'country': 'us'
+};
+
+var MARKER_PATH = 'https://developers.google.com/maps/documentation/javascript/images/marker_green';
+var hostnameRegexp = new RegExp('^https?://.+?/');
+
+var countries = {
+  'au': {
+    center: {
+      lat: -25.3,
+      lng: 133.8
+    },
+    zoom: 4
+  },
+  'br': {
+    center: {
+      lat: -16.5,
+      lng: -51.9
+    },
+    zoom: 4
+  },
+  'ca': {
+    center: {
+      lat: 62,
+      lng: -110.0
+    },
+    zoom: 3
+  },
+  'fr': {
+    center: {
+      lat: 46.9,
+      lng: 2.2
+    },
+    zoom: 6
+  },
+  'de': {
+    center: {
+      lat: 51.2,
+      lng: 10.4
+    },
+    zoom: 5
+  },
+  'mx': {
+    center: {
+      lat: 23.6,
+      lng: -102.5
+    },
+    zoom: 4
+  },
+  'nz': {
+    center: {
+      lat: -40.9,
+      lng: 174.9
+    },
+    zoom: 5
+  },
+  'it': {
+    center: {
+      lat: 41.9,
+      lng: 12.6
+    },
+    zoom: 5
+  },
+  'za': {
+    center: {
+      lat: -30.6,
+      lng: 22.9
+    },
+    zoom: 5
+  },
+  'es': {
+    center: {
+      lat: 40,
+      lng: -4.7
+    },
+    zoom: 6
+  },
+  'pt': {
+    center: {
+      lat: 39.4,
+      lng: -8.2
+    },
+    zoom: 6
+  },
+  'us': {
+    center: {
+      lat: 37.1,
+      lng: -95.7
+    },
+    zoom: 4
+  },
+  'uk': {
+    center: {
+      lat: 54.8,
+      lng: -4.6
+    },
+    zoom: 5
+  }
+};
+$(document).ready(function() {
+
+  // pop-up window for a place
+  infoWindow = new google.maps.InfoWindow({
+    content: document.getElementById('info-content')
+  });
+
+  // Create the autocomplete object and associate it with the UI input control.
+  // Restrict the search to the default country, and to place type "cities".
+  autocomplete = new google.maps.places.Autocomplete(
+    /** @type {!HTMLInputElement} */
+    (
+      document.getElementById('autocomplete')), {
+      types: ['(cities)'],
+      componentRestrictions: countryRestrict
     });
-    var marker = new google.maps.Marker({
-      position: boulder,
-      map: map
+  places = new google.maps.places.PlacesService(map);
+
+  autocomplete.addListener('place_changed', onPlaceChanged);
+
+  // Add a DOM event listener to react when the user selects a country.
+  document.getElementById('country').addEventListener(
+    'change', setAutocompleteCountry);
+
+  // When the user selects a city, get the place details for the city and
+  // zoom the map in on the city.
+  function onPlaceChanged() {
+    var place = autocomplete.getPlace();
+    if (place.geometry) {
+      map.panTo(place.geometry.location);
+      map.setZoom(13);
+      search();
+    } else {
+      document.getElementById('autocomplete').placeholder = 'Enter a city';
+    }
+  }
+
+  // Search for places in the selected city, within the viewport of the map.
+  function search() {
+    var search = {
+      bounds: map.getBounds(),
+      types: [$("#placeType option:selected").text()]
+    };
+    if (search.types == 'event') {
+      getEvents()
+    }
+    // if (search.types == 'trail') {
+    //   getTrails()
+    // }
+    places.nearbySearch(search, function(results, status) {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        clearResults();
+        //clearMarkers();
+        // Create a marker for each place found, and
+        // assign a letter of the alphabetic to each marker icon.
+        for (var i = 0; i < 14; i++) {
+          var markerLetter = String.fromCharCode('A'.charCodeAt(0) + (i % 26));
+          var markerIcon = MARKER_PATH + markerLetter + '.png';
+          // Use marker animation to drop the icons incrementally on the map.
+          // markers[i] = new google.maps.Marker({
+          //   position: results[i].geometry.location,
+          //   animation: google.maps.Animation.DROP,
+          //   icon: markerIcon
+          // });
+          // // If the user clicks a hotel marker, show the details of that hotel
+          // // in an info window.
+          // markers[i].placeResult = results[i];
+          // google.maps.event.addListener(markers[i], 'click', showInfoWindow);
+          // setTimeout(dropMarker(i), i * 100);
+          addResult(results[i], i);
+        }
+      }
     });
   }
+
+  function clearMarkers() {
+    for (var i = 0; i < markers.length; i++) {
+      if (markers[i]) {
+        markers[i].setMap(null);
+      }
+    }
+    markers = [];
+  }
+
+  // Set the country restriction based on user input.
+  // Also center and zoom the map on the given country.
+  function setAutocompleteCountry() {
+    var country = document.getElementById('country').value;
+    if (country == 'all') {
+      autocomplete.setComponentRestrictions({
+        'country': []
+      });
+      map.setCenter({
+        lat: 15,
+        lng: 0
+      });
+      map.setZoom(2);
+    } else {
+      autocomplete.setComponentRestrictions({
+        'country': country
+      });
+      map.setCenter(countries[country].center);
+      map.setZoom(countries[country].zoom);
+    }
+    clearResults();
+    // clearMarkers();
+  }
+
+  function dropMarker(i) {
+    return function() {
+      markers[i].setMap(map);
+    };
+  }
+
+  // if user selects trail from drop down
+  $("#subForm").submit(function(event) {
+    //if ($("#placeType option:selected").text() == "trail") {
+    event.preventDefault();
+    clearResults();
+    var place2 = autocomplete.getPlace();
+    var latitude = place2.geometry.location.lat()
+    var longitude = place2.geometry.location.lng()
+
+    $.get(`/apiTrail?lat=${latitude}&lon=${longitude}`, function(body, status) {
+      body = JSON.parse(body);
+      //console.log(body)
+      var result = []
+      for (var i = 0; i < 10; i++) {
+        result.push({
+          name: body.trails[i].name,
+          rating: body.trails[i].stars,
+          geometry: {
+            location: {
+              lat: parseFloat(body.trails[i].latitude),
+              lng: parseFloat(body.trails[i].longitude),
+            }
+          }
+        })
+        addResult(result[i], i);
+      }
+    })
+  })
+
+  $("#eventsForm").submit(function(event) {
+    //if ($("#placeType option:selected").text() == "trail") {
+    event.preventDefault();
+    clearResults();
+    var place = autocomplete.getPlace();
+    var location = place.address_components[0].long_name
+    var keyword = $("#eventType option:selected").text()
+    var date = $("#eventTime option:selected").text()
+    console.log(location)
+    console.log(keyword)
+    console.log(date)
+
+  //   var date = document.getElementById("date").value;
+    //   $.get(`/apiEvent?keywords=${keywords}&location=${place}&date=${date}`, function(body, status) {
+    $.get(`/apiEvent?keywords=${keyword}&location=${location}&date=${date}`, function(body, status) {
+      body = JSON.parse(body);
+      //console.log(body)
+      var result = []
+      for (var i = 0; i < 10; i++) {
+        result.push({
+          name: body.events.event[i].title,
+          rating: "",
+          start_time: body.events.event[i].start_time,
+          geometry: {
+            location: {
+              lat: parseFloat(body.events.event[i].latitude),
+              lng: parseFloat(body.events.event[i].longitude),
+            }
+          }
+        })
+
+        addResult(result[i], i);
+        // console.log(body.events.event[i].title)
+        // console.log(body.events.event[i].start_time)
+      }
+    })
+  })
+
+  function addResult(result, i) {
+    var results = document.getElementById('results');
+    var markerLetter = String.fromCharCode('A'.charCodeAt(0) + (i % 26));
+    var markerIcon = MARKER_PATH + markerLetter + '.png';
+    var tr = document.createElement('tr');
+    tr.style.backgroundColor = (i % 2 === 0 ? '#F0F0F0' : '#FFFFFF');
+    // when user clicks on result, add it to map
+    tr.onclick = function(evt) {
+      google.maps.event.trigger(markers[i], 'click');
+      markers[i] = new google.maps.Marker({
+        position: result.geometry.location,
+        animation: google.maps.Animation.DROP,
+        icon: markerIcon
+      });
+      //console.log("posResult: " + result.geometry.location)
+      // If the user clicks a marker, show the details of that marker
+      // in an info window.
+      markers[i].placeResult = result;
+      google.maps.event.addListener(markers[i], 'click', showInfoWindow);
+      setTimeout(dropMarker(i), i * 100);
+    };
+
+    var iconTd = document.createElement('td');
+    var nameTd = document.createElement('td');
+    var ratingTd = document.createElement('td');
+
+    var icon = document.createElement('img');
+    icon.src = markerIcon;
+    icon.setAttribute('class', 'placeIcon');
+    icon.setAttribute('className', 'placeIcon');
+
+    var name = document.createTextNode(result.name);
+    var rating = document.createTextNode("\nRating: " + result.rating);
+
+    iconTd.appendChild(icon);
+    nameTd.appendChild(name);
+    nameTd.appendChild(rating);
+
+    tr.appendChild(iconTd);
+    tr.appendChild(nameTd);
+    tr.appendChild(ratingTd);
+
+    results.appendChild(tr);
+  }
+
+  // if ($("#placeType option:selected").text() == "trail") {
+  //     markers[i] = new google.maps.Marker({
+  //     position: result.geometry.location,
+  //     animation: google.maps.Animation.DROP,
+  //     icon: markerIcon
+  //   });
+  //}
+
+  function clearResults() {
+    var results = document.getElementById('results');
+    while (results.childNodes[0]) {
+      results.removeChild(results.childNodes[0]);
+    }
+  }
+
+  // Get the place details for a place. Show the information in an info window,
+  // anchored on the marker for the place that the user selected.
+  function showInfoWindow() {
+    var marker = this;
+    places.getDetails({
+        placeId: marker.placeResult.place_id
+      },
+      function(place, status) {
+        if (status !== google.maps.places.PlacesServiceStatus.OK) {
+          return;
+        }
+        infoWindow.open(map, marker);
+        buildIWContent(place);
+      });
+  }
+
+  // Load the place information into the HTML elements used by the info window.
+  function buildIWContent(place) {
+    document.getElementById('iw-icon').innerHTML = '<img class="hotelIcon" ' +
+      'src="' + place.icon + '"/>';
+    document.getElementById('iw-url').innerHTML = '<b><a href="' + place.url +
+      '">' + place.name + '</a></b>';
+    document.getElementById('iw-address').textContent = place.vicinity;
+
+    if (place.formatted_phone_number) {
+      document.getElementById('iw-phone-row').style.display = '';
+      document.getElementById('iw-phone').textContent =
+        place.formatted_phone_number;
+    } else {
+      document.getElementById('iw-phone-row').style.display = 'none';
+    }
+
+    // Assign a five-star rating to the hotel, using a black star ('&#10029;')
+    // to indicate the rating the hotel has earned, and a white star ('&#10025;')
+    // for the rating points not achieved.
+    if (place.rating) {
+      var ratingHtml = '';
+      for (var i = 0; i < 5; i++) {
+        if (place.rating < (i + 0.5)) {
+          ratingHtml += '&#10025;';
+        } else {
+          ratingHtml += '&#10029;';
+        }
+        document.getElementById('iw-rating-row').style.display = '';
+        document.getElementById('iw-rating').innerHTML = ratingHtml;
+      }
+    } else {
+      document.getElementById('iw-rating-row').style.display = 'none';
+    }
+
+    // The regexp isolates the first part of the URL (domain plus subdomain)
+    // to give a short URL for displaying in the info window.
+    if (place.website) {
+      var fullUrl = place.website;
+      var website = hostnameRegexp.exec(place.website);
+      if (website === null) {
+        website = 'http://' + place.website + '/';
+        fullUrl = website;
+      }
+      document.getElementById('iw-website-row').style.display = '';
+      document.getElementById('iw-website').textContent = website;
+    } else {
+      document.getElementById('iw-website-row').style.display = 'none';
+    }
+  }
+
+
+})
+
+// $("#form").submit(function(event) {
+//   event.preventDefault();
+//   var keywords = document.getElementById("eventType").value;
+//   var location = document.getElementById("location").value;
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
 const request = require('request');
-
+//var appKey = pcXqHp3KG3jmtgNJ
 app.use(express.static('./public'))
 
 app.use(bodyParser.urlencoded({
@@ -13,15 +13,30 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json())
 
-app.get('/api', function(req, res) {
-  console.log(req.query);
-  var breweryUrl = `http://api.brewerydb.com/v2/locations?locality=${req.query.locality}&region=${req.query.region}&key=58bc55fe9138082bf63a6f6ff8c1c861`;
-  request(breweryUrl, function(err, response, body) {
+app.get('/apiTrail', function(req, res) {
+  var trailUrl = `https://www.hikingproject.com/data/get-trails?lat=${req.query.lat}&lon=${req.query.lon}&maxDistance=10&key=200192113-0e12500ca3d4423414d88aaa658cda2e`
+  //var eventUrl = `http://api.eventful.com/json/events/search?app_key=pcXqHp3KG3jmtgNJ&keywords=${req.query.keywords}&location=${req.query.location}&date=${req.query.date}`
+  request(trailUrl, function(err, response, body) {
     console.log("started API request");
+    //console.log("req.query="+req.query);
+    //console.log(response)
+    //console.log(body)
     res.send(body)
   })
 })
 
-app.listen(80, function() {
+app.get('/apiEvent', function(req, res) {
+  //var eventUrl = `http://api.eventful.com/json/events/search?app_key=pcXqHp3KG3jmtgNJ&q=music&l=Boulder+Denver&t=This+weekend`
+  //var eventUrl = `http://api.eventful.com/json/events/search?app_key=pcXqHp3KG3jmtgNJ&keywords=${req.query.keywords}&location=${req.query.location}&date=${req.query.date}`
+  var eventUrl = `http://api.eventful.com/json/events/search?keywords=${req.query.keywords}&location=${req.query.location}&date=${req.query.date}&sort_order=popularity&app_key=pcXqHp3KG3jmtgNJ`
+  request(eventUrl, function(err, response, body) {
+    console.log("started API request");
+    //console.log("req.query="+req.query);
+    //console.log(response)
+    res.send(body)
+  })
+})
+
+app.listen(8080, function() {
   console.log('The app is running on 8080');
 });
